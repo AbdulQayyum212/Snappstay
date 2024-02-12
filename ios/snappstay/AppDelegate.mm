73a0e5ff7b2adc2a98fdf000c500ed09d405d1c8
AbdulQayyum212 <aq277162@gmail.com>
2024-02-12 23:01:51 +0500

    Add: SplashScreen

--- ios/snappstay/AppDelegate.mm
+++ ios/snappstay/AppDelegate.mm
@@ -7,15 +7,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 
- 
-
+  self.moduleName = @"snappstay";
   [GMSServices provideAPIKey:@"AIzaSyCSEEKrvzM3-vFcLEoOUf256gzLG7tyWWc"]; // add this line using the api key obtained from Google Console
 
-  self.moduleName = @"snappstay";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-bool didFinish = [super application:application didFinishLaunchingWithOptions:launchOptions];
+  bool didFinish=[super application:application didFinishLaunchingWithOptions:launchOptions];
   
   [RNSplashScreen show];  // this needs to be called after [super application:application didFinishLaunchingWithOptions:launchOptions];
   
